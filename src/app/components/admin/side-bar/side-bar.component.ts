19507975316ca99cc7160b0bf9b8fc4b8a08acb3
Xanth-64 <30258079@correo.unimet.edu.ve>
2020-11-19 07:44:34 -0400

    Sidebar colocada en posición

--- src/app/components/admin/side-bar/side-bar.component.ts
+++ src/app/components/admin/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent implements OnInit {
-
-  constructor() { }
-
+  currentRoute: string
+  constructor(private route: Router) { }
+  petitionParser: string = '';
+  productParser: string = '';
+  catalogParser: string = '';
   ngOnInit(): void {
+    this.currentRoute = this.route.url.slice(7)
+    switch (this.currentRoute) {
+      case 'products':
+        this.productParser = 'active'; 
+        break;
+      case 'petitions':
+        this.petitionParser = 'active';
+        break;
+      case 'categories':
+          this.catalogParser = 'active'
+          break;
+    
+      
+    }
   }
 
 }
