620f65cdb8edcb378d05123209b7e05ba9efd3ed
Manuel Gómez <gomezmanuel1309@gmail.com>
2020-12-09 13:25:35 -0400

    vista homepage 70% implementada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,6 +48,9 @@ import { PaymentFinishComponent } from './pages/payment-finish/payment-finish.co
 import { CartBagViewComponent } from './components/cart-bag-view/cart-bag-view.component';
 import { CartProductViewComponent } from './components/cart-product-view/cart-product-view.component';
 import { CartProductViewElementComponent } from './components/cart-product-view-element/cart-product-view-element.component';
+import { MainTitleComponent } from './components/homepage/main-title/main-title.component';
+import { ProductCardComponent } from './components/homepage/product-card/product-card.component';
+import { FeaturedProductsComponent } from './components/homepage/featured-products/featured-products.component';
 
 @NgModule({
   declarations: [
@@ -86,6 +89,9 @@ import { CartProductViewElementComponent } from './components/cart-product-view-
     CartBagViewComponent,
     CartProductViewComponent,
     CartProductViewElementComponent,
+    MainTitleComponent,
+    ProductCardComponent,
+    FeaturedProductsComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Categories } from 'src/app/models/categories';
+import { Product } from 'src/app/models/product';
+import { CategoriesService } from 'src/app/service/admin-crud/categories.service';
+import { ProductosService } from 'src/app/services/admin-crud/productos.service';
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
+products: Array<Product>
+  constructor( public productsservices: ProductosService, public categoryService: CategoriesService) { }
 
   ngOnInit(): void {
+    this.getAllProducts()
   }
-
+  getAllProducts():void{
+  this.productsservices.getAllProducts().subscribe((items) => {
+  this.products = items.map((item) =>({...item.payload.doc.data(),
+  $key: item.payload.doc.id } as Product)).filter((product) => {
+   this.categoryService.getAllCategories().subscribe((categoryy) =>{
+    categoryy.map((cat) =>({...cat.payload.doc.data(), $key: cat.payload.doc.id }as Categories)).forEach(
+      (catValue,index)=>{
+        if (catValue.productsIds.length != 0) {
+          if (catValue.productsIds[0] == product.$key) {
+            if (!this.products.includes(product)) {
+              this.products.push(product)
+            }
+            
+          }
+          
+        }})})})})}
 }
